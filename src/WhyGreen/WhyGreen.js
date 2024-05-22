3cb505bb894588bfe31ff55d563b843cce61d0cb
Srikark-17 <srikar.sgk@gmail.com>
2024-05-21 19:51:34 -0500

    improved why go green section

--- src/WhyGreen/WhyGreen.js
+++ src/WhyGreen/WhyGreen.js
@@ -44,7 +44,7 @@ const WhyGreen = () => {
           <svg
             data-name="Layer 1"
             xmlns="http://www.w3.org/2000/svg"
-            width="13vw"
+            width="16vw"
             viewBox="0 0 881.63115 587.66492"
           >
             <polygon
